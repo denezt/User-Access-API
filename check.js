4e16686b4bba4213d677d106ce11e65f2c1f79ba
Richard L. Jackson <denezt@yahoo.com>
2017-11-13 13:17:17 -0600

    Adding compatibility for Apple Safari

--- check.js
+++ check.js
@@ -1,24 +1,66 @@
 var form = document.querySelector('form');
 
-form.addEventListener('submit', function (event) {
-   event.preventDefault();
-   var data = new FormData(form);
-   check(data);
-});
+if (!isSafariBrowser()){
+	form.addEventListener('submit', function (event) {
+   		event.preventDefault();
+		var data = new FormData(form);
+		check(data);
+	});
+} else {
+	form.addEventListener('submit', function (event) {
+		event.preventDefault();
+		var x = document.getElementById("username").value;
+		console.info("Username: " + x);
+		console.info("Running, checks...");
+		checkSafari(x);
+	});
+}
 
-function check (data){
+// Default
+function check(data){
    var request = new XMLHttpRequest();
    var results = data.get('username');
    var query_request = "usernamecheck.php?username=" + results;
-   request.open("POST", query_request);
+   request.open("GET", query_request);
    request.addEventListener('load', function(event) {
-      if (request.status >= 200 && request.status < 300) {
-         console.log(request.responseText);
-         console.log("Return Code: " + request.status);
-      } else {
-         console.warn(request.statusText, request.responseText);
-      }
+	var d = new Date();
+	if (request.status >= 200 && request.status < 300) {
+        	console.log(request.responseText);
+        	console.log("Return Code: " + request.status);
+		messageStatus("Sent, " + d);
+      	} else {
+        	console.warn(request.statusText, request.responseText);
+		messageStatus("Error Occurred");
+      	}
    });
-   console.log(results);
    request.send(data);
+}
+
+// Only for Safari - Apple
+function checkSafari(input_data){
+	var request = new XMLHttpRequest();
+	var results = input_data;
+	var query_request = "usernamecheck.php?username=" + results;
+	request.open("GET", query_request, true);
+   	request.addEventListener('load', function(event) {
+	var d = new Date();
+	if (request.status >= 200 && request.status < 300) {
+        	console.log(request.responseText);
+        	console.log("Return Code: " + request.status);
+		messageStatus("Sent, " + d);
+      	} else {
+        	console.warn(request.statusText, request.responseText);
+		messageStatus("Error Occurred");
+      	}
+   });
+   request.send();
+}
+
+function isSafariBrowser(){
+	var VendorName = window.navigator.vendor;
+	return ((VendorName.indexOf('Apple') > -1) && (window.navigator.userAgent.indexOf('Safari') > -1));
+	}
+
+function messageStatus(status_in){
+	document.getElementById("msg_status").innerHTML = status_in;
 }
--- usernamecheck.php
+++ usernamecheck.php
@@ -1,29 +1,33 @@
 <?php
 $username = $_GET["username"];
+$status = "pass";
 
 $responseStatus = '200 OK';
-$responseText = 'Richard Hackson';
-if (!isset($_POST['username'])) {
+$responseText = 'Null';
+if (!isset($_GET['username'])) {
    $responseStatus = '400 Bad Request';
    $responseText = 'Query does not have a username';
+   $status = "fail";
 } else {
    $usernames = array('admin','gast','paul');
    $validatePattern = '/^[A-z0-9]{4,20}$/';
-   if (in_array($_POST['username'],$usernames)) {
+   if (in_array($_GET['username'],$usernames)) {
       $responseStatus = '409 Conflict';
       $responseText = 'Username already exists';
-   } elseif (!preg_match($validatePattern,$_POST['username'])) {
+      $status = "fail";
+   } elseif (!preg_match($validatePattern,$_GET['username'])) {
       $responseStatus = '400 Bad Request';
       $responseText = 'Username does not follow the secure policy.';
+      $status = "fail";
    } else {
       $reponseStatus = '204 No Content';
+      $status = "pass";
    }
 }
-$data = [ 'fname' => $username, 'lname' => 'Smith', 'age' => 25 ];
+
+$data = [ 'fname' => $username, 'lname' => 'Smith', 'age' => 25, 'Status' => $status, 'StatusCode' => $responseStatus ];
 
 header($_SERVER['SERVER_PROTOCOL'].' '.$responseStatus);
-# header('Content-type: text/html; charset=utf-8');
-# echo $responseText;
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($data);
 
